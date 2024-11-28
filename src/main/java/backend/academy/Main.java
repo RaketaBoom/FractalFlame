48bc6443d4952125f3c3471bce6abf5e0831ae18
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-11-29 01:34:18 +0400

    feat: add launch classes

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,12 +1,22 @@
 package backend.academy;
 
 import backend.academy.fractalflame.FractalFlameApi;
+import backend.academy.fractalflame.generator.FlameHistogramGenerator;
+import backend.academy.fractalflame.loader.JsonConfigLoader;
+import backend.academy.fractalflame.service.FractalFlameService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Random;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+    //TODO продумать бизнесовку с учетом в дальнейшем выбора многопоточки и однопоточки
     public static void main(String[] args) {
-        FractalFlameApi fractalFlameApi = new FractalFlameApi();
+        Random random = new Random();
+
+        FractalFlameApi fractalFlameApi = new FractalFlameApi(new JsonConfigLoader(new ObjectMapper()),
+            new FractalFlameService(new FlameHistogramGenerator(random)));
+
         fractalFlameApi.start();
     }
 }
---
+++ src/main/java/backend/academy/fractalflame/FractalFlameApi.java
@@ -0,0 +1,35 @@
+package backend.academy.fractalflame;
+
+import backend.academy.fractalflame.constant.Constants;
+import backend.academy.fractalflame.dto.ConfigDto;
+import backend.academy.fractalflame.dto.HistogramRequest;
+import backend.academy.fractalflame.enums.ImageFormat;
+import backend.academy.fractalflame.factory.HistogramRequestFactory;
+import backend.academy.fractalflame.generator.FileNameGenerator;
+import backend.academy.fractalflame.loader.JsonConfigLoader;
+import backend.academy.fractalflame.model.FractalImage;
+import backend.academy.fractalflame.service.FractalFlameService;
+import backend.academy.fractalflame.utils.ImageUtils;
+import java.nio.file.Path;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FractalFlameApi {
+    private final JsonConfigLoader configLoader;
+    private final FractalFlameService service;
+
+    public void start() {
+        ConfigDto configDto = configLoader.loadConfig(Path.of(Constants.CONFIG_PATH));
+
+        HistogramRequest histogramRequest = HistogramRequestFactory.createFromConfig(configDto);
+        ImageFormat format = ImageFormat.fromString(configDto.format());
+
+        FractalImage fractalImage = service.createFlameHistogram(histogramRequest);
+
+        saveImage(fractalImage, Constants.OUTPUT_DIRECTORY, format);
+    }
+
+    private void saveImage(FractalImage fractalImage, String outputDirectory, ImageFormat format) {
+        ImageUtils.save(fractalImage, Path.of(outputDirectory, FileNameGenerator.dataTimeName()), format);
+    }
+}
