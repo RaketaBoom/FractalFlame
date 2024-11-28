630a161e4947ed07725fb1f1e6f6b14d879356b2
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-11-29 01:11:12 +0400

    feat: add symmetry, heart and affine transformations

---
+++ src/main/java/backend/academy/fractalflame/transformation/Transformation.java
@@ -0,0 +1,7 @@
+package backend.academy.fractalflame.transformation;
+
+import backend.academy.fractalflame.model.Point;
+import java.util.function.Function;
+
+public interface Transformation extends Function<Point, Point> {
+}
