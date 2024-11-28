252312a813175ab85a75bf051c74c943aedd7804
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-11-29 01:08:32 +0400

    fix: add validation methods for configDto

--- src/main/java/backend/academy/fractalflame/validator/ConfigValidator.java
+++ src/main/java/backend/academy/fractalflame/validator/ConfigValidator.java
@@ -1,8 +1,15 @@
 package backend.academy.fractalflame.validator;
 
+import backend.academy.fractalflame.constant.Constants;
 import backend.academy.fractalflame.dto.ConfigDto;
+import backend.academy.fractalflame.dto.VariationDto;
+import backend.academy.fractalflame.enums.ImageFormat;
+import backend.academy.fractalflame.exception.EmptyFormatFieldException;
+import backend.academy.fractalflame.exception.EmptyVariationListException;
 import backend.academy.fractalflame.exception.IllegalIterationsException;
 import backend.academy.fractalflame.exception.IllegalSizeException;
+import backend.academy.fractalflame.exception.IllegalSymmetryException;
+import java.util.List;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -10,6 +17,9 @@ public class ConfigValidator {
     public static void validate(ConfigDto configDto) {
         validateSize(configDto.height(), configDto.width());
         validateIterations(configDto.iterations());
+        validateSymmetry(configDto.symmetry());
+        validateVariations(configDto.variations());
+        validateFormat(configDto.format());
     }
 
     private static void validateSize(int height, int width) {
@@ -19,8 +29,26 @@ public class ConfigValidator {
     }
 
     private static void validateIterations(int iterations) {
-        if (iterations <= 0) {
+        if (iterations <= Constants.WARMUP_ITERATIONS) {
             throw new IllegalIterationsException(iterations);
+        }
+    }
+
+    private static void validateSymmetry(int symmetry) {
+        if (symmetry <= 0) {
+            throw new IllegalSymmetryException(symmetry);
+        }
+    }
+
+    private static void validateVariations(List<VariationDto> variationDtos) {
+        if (variationDtos == null || variationDtos.isEmpty()) {
+            throw new EmptyVariationListException();
+        }
+    }
+
+    private static void validateFormat(String name) {
+        if (name == null || name.isBlank()) {
+            throw new EmptyFormatFieldException();
         }
     }
 }
