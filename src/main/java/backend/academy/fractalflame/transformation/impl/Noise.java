5952d49f397113ca4bbee5760a2d9b88b6171bd7
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-12-12 11:39:45 +0400

    fix: replace magic numbers with the constant

--- src/main/java/backend/academy/fractalflame/transformation/impl/Noise.java
+++ src/main/java/backend/academy/fractalflame/transformation/impl/Noise.java
@@ -4,13 +4,16 @@ import backend.academy.fractalflame.model.Point;
 import backend.academy.fractalflame.transformation.Transformation;
 
 public class Noise implements Transformation {
+
+    public static final double NOISE_VALUE = 0.5;
+
     @Override
     public Point apply(Point point) {
         double x = point.x();
         double y = point.y();
 
-        double noiseX = Math.random() - 0.5;
-        double noiseY = Math.random() - 0.5;
+        double noiseX = Math.random() - NOISE_VALUE;
+        double noiseY = Math.random() - NOISE_VALUE;
 
         return new Point(x + noiseX, y + noiseY);
     }
